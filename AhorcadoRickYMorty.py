ab58afabec1c6dff8ceef19d6178bdeb25a5961f
saragestt <iamsaramusic@gmail.com>
2025-04-18 16:08:06 +0200

    no consigo cambiar las rayas por las letras

--- AhorcadoRickYMorty.py
+++ AhorcadoRickYMorty.py
@@ -15,35 +15,32 @@ especie = objeto["species"]
 tipo = objeto["type"]
 genero = objeto["gender"]
 foto = objeto["image"]
-
 print(nombre)
 
-
-
-
 print(f"Ahorcado de Rick y Morty!\n"
-      f"Adivina el personaje a partir de estas pistas: {especie}, {tipo} y {genero} \n"
+      f"Adivina el personaje a partir de estas pistas: {especie}, {tipo} y {genero}. \n"
       f"Tienes 6 intentos")
 
+contador = 0
+
 personaje_oculto = []
-for i in range(len(personaje)):
+for i in range(len(nombre)):
     personaje_oculto.append("_")
 print(personaje_oculto)
 
-contador = 0
-introduce = input(f"Introduce una letra: ")
+
 
 while contador < 6:
     print(f"Intentos: {contador}")
     letra = input(f"Introduce una letra: ")
 
-    if letra in personaje:
-        print("Letra correcta!") #No se porque esto no se imprime
+    if letra in nombre:
+        print("Letra correcta!") 
         print(personaje_oculto)
 
     else:
         contador += 1
-        print("Incorrecto!") #no se porque aveces cuenta como error algunas letras que estan en el nombre y otras veces no
+        print("Incorrecto!")
 
 
 if "_" not in personaje_oculto:
